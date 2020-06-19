4ba73e0389f38d96e691116c80a4f4c1d76d6d8a
rcfgama <rcfgama@gmail.com>
2020-06-19 18:49:41 -0300

    subindo o refresher

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -20,6 +20,8 @@ export class FeedPage {
 
   public lista_filmes = new Array<any>();
   public loading;
+  public event;
+  public refreshing: boolean = false;
 
   constructor(
     private provide: ProviderService,
@@ -35,17 +37,35 @@ export class FeedPage {
      await this.loading.dismiss();
   }
 
+  doRefresh(event) {
+    this.event = event;
+    this.refreshing = true;
+
+    this.carregarFilmes();
+  }
+
   ionViewDidEnter() { 
+    this.carregarFilmes();
+  }
+
+  carregarFilmes() {
     this.presentLoading();
     this.provide.getLatestMovies().subscribe(
       data => {
         const response = (data as any);
         this.lista_filmes = response.results;
         console.log(response);
+        if(this.refreshing) {
+          this.event.target.complete();
+          this.refreshing = false;
+        }
       }, error => {
         console.log(error);
+        if(this.refreshing) {
+          this.event.target.complete();
+          this.refreshing = false;
+        }
       }
     )
   }
-
 }
