00f9e9766f4086fe8d630897bcf734e65b399440
rcfgama <rcfgama@gmail.com>
2020-06-20 15:07:23 -0300

    subindo a camera

--- src/app/home/tab1.page.ts
+++ src/app/home/tab1.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers: [
+    Camera
+  ]
 })
 export class Tab1Page {
+  img = "";
 
-  constructor() {}
+  constructor(
+    private camera: Camera
+  ) {}
 
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
 }
